36a5ade7a2952fd1bb47769ea9555b2f387ff8a9
Pierce <ti436522@gmail.com>
2021-09-07 17:19:09 +0100

    Load the Data set

--- index.py
+++ index.py
@@ -4,3 +4,12 @@ import numpy as np
 
 # For Data Processing
 import pandas as pd
+
+#Load the data set
+df = pd.read_csv('E:/College/Semester 7/AI/Rain Detector App/weatherAUS.csv')
+
+#Display the shape of the data set
+print('Size of weather data frame is :',df.shape)
+
+#Display data rows from 0 to 5
+print(df[0:5])
